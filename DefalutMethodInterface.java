657ef13edba772d04d24123edbe5110e7d41f51c
Nitish <nkgupta3541@gmail.com>
2020-03-14 20:26:28 +0530

    Default Method Interface implementaion outside

--- DefalutMethodInterface.java
+++ DefalutMethodInterface.java
@@ -6,11 +6,15 @@ interface interf
 	}
 }
 
-class DefaultMethodInterface implements interf
+class DefaultMethodInterface1 implements interf
 {
+	public void m1()
+	{
+		System.out.println("Default Method Overrideen");
+	}
 	public static void main(String[] args)
 	{
-        DefaultMethodInterface obj = new DefaultMethodInterface();
+        DefaultMethodInterface1 obj = new DefaultMethodInterface1();
         obj.m1();
 	}
 }
